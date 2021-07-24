dc5a550d325b2844b8de6cd45557406927a0a56a
Jim Conner <b_rennochj@outlook.com>
2021-07-24 08:54:25 -0500

    adding x-ray

--- push.sh
+++ push.sh
@@ -2,4 +2,8 @@ git add -A && git commit -m "adding x-ray"
 git push https://github.com/connerjh/http-echo.git
 
 docker build -t connerjh/http-echo .
-docker push connerjh/http-echo
+docker push connerjh/http-ec
+
+docker build -t http-echo .
+docker tag http-echo:latest 882103041271.dkr.ecr.us-west-2.amazonaws.com/http-echo:latest
+docker push 882103041271.dkr.ecr.us-west-2.amazonaws.com/http-echo:latest
