e01b619022f49b6313c95cf839d9c1847de9d300
Jim Conner <b_rennochj@outlook.com>
2021-07-23 22:20:35 -0500

    adding x-ray

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from json import dumps
 from time import sleep
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 from aws_xray_sdk.core import xray_recorder
+import os
 
 app = Flask(__name__)
 
@@ -21,9 +22,17 @@ def http_echo():
 
     message = {}
     headers = {}
+    environment = {}
     
     for item in request.headers.to_wsgi_list():
         headers[item[0]] = item[1]
+
+    for key in request.environ.keys():
+        value = request.environ[key]
+        if type(value) is str or type(value) is tuple or type(value) is int or type(value) is list or type(value) is dict:
+            environment[key] = value
+
+    print(environment)
 
     message["path"] = request.path
     message["full_path"] = request.full_path
@@ -39,7 +48,8 @@ def http_echo():
     message["origin"] = request.origin
     message["url_root"] = request.url_root
     message["url_charset"] = request.url_charset
-    message["environment"] = request.environ
+    message["environment"] = environment
+    message["host_ip"] = os.environ["HOST_IP"]
  
     response = make_response(dumps(message, sort_keys=True, indent=4))
     response.headers['Content-Type'] = 'application/json; charset=utf-8'
